d7a2dfc42a5b40b2be9fc8f3da15b26cd1eda23b
Owen Wang <wangfowen@gmail.com>
2018-09-30 01:17:34 -0700

    revamp into friendship intersector -- can upload and read both files

--- index.js
+++ index.js
@@ -1,256 +1,203 @@
+var firstData = [];
+var secondData = [];
+
 ( function ( $, L, prettySize ) {
-	var map, heat,
-		heatOptions = {
-			tileOpacity: 1,
-			heatOpacity: 1,
-			radius: 25,
-			blur: 15
-		};
+  var map, heat,
+    heatOptions = {
+      tileOpacity: 1,
+      heatOpacity: 1,
+      radius: 25,
+      blur: 15
+    };
 
-	function status( message ) {
-		$( '#currentStatus' ).text( message );
-	}
-	// Start at the beginning
-	stageOne();
+  function status( message ) {
+    $( '#currentStatus' ).text( message );
+  }
 
-	function stageOne () {
-		var dropzone;
+  var files = [null, null];
 
-		// Initialize the map
-		map = L.map( 'map' ).setView( [0,0], 2 );
-		L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: 'location-history-visualizer is open source and available <a href="https://github.com/theopolisme/location-history-visualizer">on GitHub</a>. Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors.',
-			maxZoom: 18,
-			minZoom: 2
-		} ).addTo( map );
+  function uploadFiles(file, idx) {
+    files[idx] = file;
 
-		// Initialize the dropzone
-		dropzone = new Dropzone( document.body, {
-			url: '/',
-			previewsContainer: document.createElement( 'div' ), // >> /dev/null
-			clickable: false,
-			accept: function ( file, done ) {
-				stageTwo( file );
-				dropzone.disable(); // Your job is done, buddy
-			}
-		} );
+    if (files[0] !== null && files[1] !== null) {
+      stageTwo(files)
+    }
+  }
 
-		// For mobile browsers, allow direct file selection as well
-		$( '#file' ).change( function () {
-			stageTwo( this.files[0] );
-			dropzone.disable();
-		} );
-	}
+  // Start at the beginning
+  stageOne();
 
-	function stageTwo ( file ) {
-    // Google Analytics event - heatmap upload file
-    ga('send', 'event', 'Heatmap', 'upload', undefined, file.size);
+  function stageOne () {
+    // Initialize the map
+    map = L.map( 'map' ).setView( [0,0], 2 );
+    L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'location-history-visualizer, which this is based off of, is available <a href="https://github.com/theopolisme/location-history-visualizer">on GitHub</a>. Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors.',
+      maxZoom: 18,
+      minZoom: 2
+    } ).addTo( map );
 
-		heat = L.heatLayer( [], heatOptions ).addTo( map );
+    $( '#file-1' ).change( function () {
+      uploadFiles(this.files[0], 0);
+    } );
+    $( '#file-2' ).change( function () {
+      uploadFiles(this.files[0], 1);
+    } );
+  }
 
-		var type;
+  function initOboe(arr, callback) {
+    var os = new oboe();
+    os.node( 'locations.*', function ( location ) {
+      var SCALAR_E7 = 0.0000001; // Since Google Takeout stores latlngs as integers
+        arr.push( [ location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7, location.timestampMs ] );
+      return oboe.drop;
+    }).done(function() {
+      //heat._latlngs = latlngs;
+      //heat.redraw();
+      callback();
+    });
 
-		try {
-			if ( /\.kml$/i.test( file.name ) ) {
-				type = 'kml';
-			} else {
-				type = 'json';
-			}
-		} catch ( ex ) {
-			status( 'Something went wrong generating your map. Ensure you\'re uploading a Google Takeout JSON file that contains location data and try again, or create an issue on GitHub if the problem persists. ( error: ' + ex.message + ' )' );
-			return;
-		}
+    return os;
+  }
 
-		// First, change tabs
-		$( 'body' ).addClass( 'working' );
-		$( '#intro' ).addClass( 'hidden' );
-		$( '#working' ).removeClass( 'hidden' );
+  function stageTwo ( files ) {
+    heat = L.heatLayer( [], heatOptions ).addTo( map );
 
-		var latlngs = [];
+    try {
+      if (!(/\.json$/i.test(files[0].name)) || !(/\.json$/i.test(files[1].name))) {
+        status( 'Something went wrong generating your map. Ensure you\'re uploading a Google Takeout JSON file that contains location data and try again, or create an issue on GitHub if the problem persists. ( error: ' + ex.message + ' )' );
+        return;
+      }
+    } catch ( ex ) {
+      status( 'Something went wrong generating your map. Ensure you\'re uploading a Google Takeout JSON file that contains location data and try again, or create an issue on GitHub if the problem persists. ( error: ' + ex.message + ' )' );
+      return;
+    }
 
-		var os = new oboe();
+    // First, change tabs
+    $( 'body' ).addClass( 'working' );
+    $( '#intro' ).addClass( 'hidden' );
+    $( '#working' ).removeClass( 'hidden' );
 
-		os.node( 'locations.*', function ( location ) {
-			var SCALAR_E7 = 0.0000001; // Since Google Takeout stores latlngs as integers
-			if ( type === 'json' ) latlngs.push( [ location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7 ] );
-			return oboe.drop;
-		} ).done( function () {
-			status( 'Generating map...' );
-			heat._latlngs = latlngs;
 
-			heat.redraw();
-			stageThree(  /* numberProcessed */ latlngs.length );
+    var fileSizeOne = prettySize(files[0].size);
+    var fileSizeTwo = prettySize(files[1].size);
 
-		} );
+    status( 'Preparing to import files ( ' + fileSizeOne + ' and ' + fileSizeTwo + ' )...' );
 
-		var fileSize = prettySize( file.size );
+    parseJSONFile(files[0], initOboe(firstData, function() {
+      status('Uploading second...');
+      parseJSONFile(files[1], initOboe(secondData, function() {
+        status('Generating map...');
+        stageThree(  /* numberProcessed */ firstData.length + secondData.length );
+      }));
+    }));
+  }
 
-		status( 'Preparing to import file ( ' + fileSize + ' )...' );
-
-		// Now start working!
-		if ( type === 'json' ) parseJSONFile( file, os );
-		if ( type === 'kml' ) parseKMLFile( file );
-	}
-
-	function stageThree ( numberProcessed ) {
+  function stageThree ( numberProcessed ) {
     // Google Analytics event - heatmap render
     ga('send', 'event', 'Heatmap', 'render', undefined, numberProcessed);
 
-		var $done = $( '#done' );
+    var $done = $( '#done' );
 
-		// Change tabs :D
-		$( 'body' ).removeClass( 'working' );
-		$( '#working' ).addClass( 'hidden' );
-		$done.removeClass( 'hidden' );
+    // Change tabs :D
+    $( 'body' ).removeClass( 'working' );
+    $( '#working' ).addClass( 'hidden' );
+    $done.removeClass( 'hidden' );
 
-		// Update count
-		$( '#numberProcessed' ).text( numberProcessed.toLocaleString() );
+    // Update count
+    $( '#numberProcessed' ).text( numberProcessed.toLocaleString() );
 
-    $( '#launch' ).click( function () {
-      var $email = $( '#email' );
-      if ( $email.is( ':valid' ) ) {
-        $( this ).text( 'Launching... ' );
-        $.post( '/heatmap/submit-email.php', {
-          email: $email.val()
-        } )
-        .always( function () {
-          $( 'body' ).addClass( 'map-active' );
-          $done.fadeOut();
-          activateControls();
-        } );
-      } else {
-        alert( 'Please enter a valid email address to proceed.' );
+    $( '#launch' ).click(function () {
+      /*
+      $( 'body' ).addClass( 'map-active' );
+      $done.fadeOut();
+      activateControls();
+      */
+      console.log(firstData);
+      console.log(secondData);
+    });
+
+    function activateControls () {
+      var $tileLayer = $( '.leaflet-tile-pane' ),
+        $heatmapLayer = $( '.leaflet-heatmap-layer' ),
+        originalHeatOptions = $.extend( {}, heatOptions ); // for reset
+
+      // Update values of the dom elements
+      function updateInputs () {
+        var option;
+        for ( option in heatOptions ) {
+          if ( heatOptions.hasOwnProperty( option ) ) {
+            document.getElementById( option ).value = heatOptions[option];
+          }
+        }
       }
-    } );
 
-		function activateControls () {
-			var $tileLayer = $( '.leaflet-tile-pane' ),
-				$heatmapLayer = $( '.leaflet-heatmap-layer' ),
-				originalHeatOptions = $.extend( {}, heatOptions ); // for reset
+      updateInputs();
 
-			// Update values of the dom elements
-			function updateInputs () {
-				var option;
-				for ( option in heatOptions ) {
-					if ( heatOptions.hasOwnProperty( option ) ) {
-						document.getElementById( option ).value = heatOptions[option];
-					}
-				}
-			}
+      $( '.control' ).change( function () {
+        switch ( this.id ) {
+          case 'tileOpacity':
+            $tileLayer.css( 'opacity', this.value );
+            break;
+          case 'heatOpacity':
+            $heatmapLayer.css( 'opacity', this.value );
+            break;
+          default:
+            heatOptions[ this.id ] = Number( this.value );
+            heat.setOptions( heatOptions );
+            break;
+        }
+      } );
 
-			updateInputs();
+      $( '#reset' ).click( function () {
+        $.extend( heatOptions, originalHeatOptions );
+        updateInputs();
+        heat.setOptions( heatOptions );
+        // Reset opacity too
+        $heatmapLayer.css( 'opacity', originalHeatOptions.heatOpacity );
+        $tileLayer.css( 'opacity', originalHeatOptions.tileOpacity );
+      } );
+    }
+  }
 
-			$( '.control' ).change( function () {
-				switch ( this.id ) {
-					case 'tileOpacity':
-						$tileLayer.css( 'opacity', this.value );
-						break;
-					case 'heatOpacity':
-						$heatmapLayer.css( 'opacity', this.value );
-						break;
-					default:
-						heatOptions[ this.id ] = Number( this.value );
-						heat.setOptions( heatOptions );
-						break;
-				}
-			} );
+  /*
+  Break file into chunks and emit 'data' to oboe instance
+  */
 
-			$( '#reset' ).click( function () {
-				$.extend( heatOptions, originalHeatOptions );
-				updateInputs();
-				heat.setOptions( heatOptions );
-				// Reset opacity too
-				$heatmapLayer.css( 'opacity', originalHeatOptions.heatOpacity );
-				$tileLayer.css( 'opacity', originalHeatOptions.tileOpacity );
-			} );
-		}
-	}
+  function parseJSONFile( file, oboeInstance ) {
+    var fileSize = file.size;
+    var prettyFileSize = prettySize(fileSize);
+    var chunkSize = 512 * 1024; // bytes
+    var offset = 0;
+    var self = this; // we need a reference to the current object
+    var chunkReaderBlock = null;
+    var readEventHandler = function ( evt ) {
+      if ( evt.target.error == null ) {
+        offset += evt.target.result.length;
+        var chunk = evt.target.result;
+        var percentLoaded = ( 100 * offset / fileSize ).toFixed( 0 );
+        status( percentLoaded + '% of ' + prettyFileSize + ' loaded...' );
+        oboeInstance.emit( 'data', chunk ); // callback for handling read chunk
+      } else {
+        return;
+      }
+      if ( offset >= fileSize ) {
+        oboeInstance.emit( 'done' );
+        return;
+      }
 
-	/*
-	Break file into chunks and emit 'data' to oboe instance
-	*/
+      // of to the next chunk
+      chunkReaderBlock( offset, chunkSize, file );
+    }
 
-	function parseJSONFile( file, oboeInstance ) {
-		var fileSize = file.size;
-		var prettyFileSize = prettySize(fileSize);
-		var chunkSize = 512 * 1024; // bytes
-		var offset = 0;
-		var self = this; // we need a reference to the current object
-		var chunkReaderBlock = null;
-		var startTime = Date.now();
-		var endTime = Date.now();
-		var readEventHandler = function ( evt ) {
-			if ( evt.target.error == null ) {
-				offset += evt.target.result.length;
-				var chunk = evt.target.result;
-				var percentLoaded = ( 100 * offset / fileSize ).toFixed( 0 );
-				status( percentLoaded + '% of ' + prettyFileSize + ' loaded...' );
-				oboeInstance.emit( 'data', chunk ); // callback for handling read chunk
-			} else {
-				return;
-			}
-			if ( offset >= fileSize ) {
-				oboeInstance.emit( 'done' );
-				return;
-			}
+    chunkReaderBlock = function ( _offset, length, _file ) {
+      var r = new FileReader();
+      var blob = _file.slice( _offset, length + _offset );
+      r.onload = readEventHandler;
+      r.readAsText( blob );
+    }
 
-			// of to the next chunk
-			chunkReaderBlock( offset, chunkSize, file );
-		}
-
-		chunkReaderBlock = function ( _offset, length, _file ) {
-			var r = new FileReader();
-			var blob = _file.slice( _offset, length + _offset );
-			r.onload = readEventHandler;
-			r.readAsText( blob );
-		}
-
-		// now let's start the read with the first block
-		chunkReaderBlock( offset, chunkSize, file );
-	}
-
-	/*
-        Default behavior for file upload (no chunking)	
-	*/
-
-	function parseKMLFile( file ) {
-		var fileSize = prettySize( file.size );
-		var reader = new FileReader();
-		reader.onprogress = function ( e ) {
-			var percentLoaded = Math.round( ( e.loaded / e.total ) * 100 );
-			status( percentLoaded + '% of ' + fileSize + ' loaded...' );
-		};
-
-		reader.onload = function ( e ) {
-			var latlngs;
-			status( 'Generating map...' );
-			latlngs = getLocationDataFromKml( e.target.result );
-			heat._latlngs = latlngs;
-			heat.redraw();
-			stageThree( latlngs.length );
-		}
-		reader.onerror = function () {
-			status( 'Something went wrong reading your JSON file. Ensure you\'re uploading a "direct-from-Google" JSON file and try again, or create an issue on GitHub if the problem persists. ( error: ' + reader.error + ' )' );
-		}
-		reader.readAsText( file );
-	}
-
-	function getLocationDataFromKml( data ) {
-		var KML_DATA_REGEXP = /<when>( .*? )<\/when>\s*<gx:coord>( \S* )\s( \S* )\s( \S* )<\/gx:coord>/g,
-			locations = [],
-			match = KML_DATA_REGEXP.exec( data );
-
-		// match
-		//  [ 1 ] ISO 8601 timestamp
-		//  [ 2 ] longitude
-		//  [ 3 ] latitude
-		//  [ 4 ] altitude ( not currently provided by Location History )
-		while ( match !== null ) {
-			locations.push( [ Number( match[ 3 ] ), Number( match[ 2 ] ) ] );
-			match = KML_DATA_REGEXP.exec( data );
-		}
-
-		return locations;
-	}
+    // now let's start the read with the first block
+    chunkReaderBlock( offset, chunkSize, file );
+  }
 
 }( jQuery, L, prettySize ) );
